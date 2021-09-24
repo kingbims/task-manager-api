720e5670350b1b6229b8a426eeee2385cb6aafc3
Moremi <moremim@nameksolutions.com>
2021-09-24 10:26:09 +0100

    Init commit

---
+++ src/emails/accounts.js
@@ -0,0 +1,34 @@
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+SibApiV3Sdk.ApiClient.instance.authentications['api-key'].apiKey = process.env.SENDINBLUE_API_KEY;
+
+
+
+const sendWelcomEmail = (email, name) => {
+  new SibApiV3Sdk.TransactionalEmailsApi().sendTransacEmail(
+    {
+      'subject':'Thanks for joining in!',
+      'sender' : {'email':'moremiabimbolamorakinyo@gmail.com'},
+      'to' : [{name, email}],
+      'textContent': `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    }
+  )
+}
+
+
+const sendCancelEmail = (email, name) => {
+  new SibApiV3Sdk.TransactionalEmailsApi().sendTransacEmail(
+    {
+      'subject':'I am sad to see you go!',
+      'sender' : {'email':'moremiabimbolamorakinyo@gmail.com'},
+      'to' : [{name, email}],
+      'textContent': `Goodbye, ${name}. I hope to see you sometime soon.`
+    }
+  )
+}
+
+
+
+module.exports = {
+  sendWelcomEmail,
+  sendCancelEmail
+}
